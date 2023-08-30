ec6d6c7e70cee50d0a526bc890bfb96aa6bd274f
mauro_genna <gennamauro18@gmail.com>
2023-08-30 12:00:36 -0300

    NavBar

--- bambu/src/pages/_app.js
+++ bambu/src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import Navbar from '@/Components/NavBar/navBar'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  )
 }

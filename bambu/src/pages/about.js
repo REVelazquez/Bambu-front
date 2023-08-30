00e39eb8bc973aaccb02158f6fbc8d2335699a34
REVelazquez <rodrigo_velazquez_664@hotmail.com>
2023-08-30 12:07:53 -0300

    Inicio about o institucional

---
+++ bambu/src/pages/about.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1 class='m-8 font-bold' >Elevando PyMES y Emprendedores hacia el Éxito</h1>
+        
+        <div class='my-4 mx-4'>
+
+        <h2 class='mx-4 font-semibold'>Sobre nosotros</h2>
+            <div class='flex flex-row'>
+                <span class="inline-flex space-x-1 w-64 flex-row text-justify">
+                    <p class='m-0'>Somos más que una empresa de consultoria y capacitación somos el viento fresco que impulsa a las PyMes y pequeños emprendedores a crecer y properar</p>
+                </span>
+                <img src='' alt='Equipo o Logo' class=' mx-8' />
+            </div>
+        </div>
+
+        <div>
+            <h2 class='mx-8'>¿Que nos hace destacar?</h2>
+            <div>
+                
+            </div>
+        </div>
+    </div>
+  );
+};

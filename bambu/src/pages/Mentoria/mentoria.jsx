fde67718c6c9191fb0deb1f1958d57e2f30d3b05
Fervolpato <fervolpato1991@gmail.com>
2023-09-01 11:07:42 -0300

    Servicio Mentorias

--- bambu/src/pages/Mentoria/mentoria.jsx
+++ bambu/src/pages/Mentoria/mentoria.jsx
@@ -2,37 +2,52 @@ import React from 'react';
 import style from './mentoria.module.css';
 
 const Mentoring = () => {
-  return (
-    <div className={style.container}>
-    <div className="flex flex-col items-center justify-center p-2 h-screen" style={{ fontFamily: 'Nunito Sans'}}>
-        <div className="w-1/2 p-10">
-      <h1 className="text-2xl font-bold mb-4">Servicios de Mentoring</h1>
-      <p className="text-center">
-        Ofrecemos servicios de mentoring personalizados para ayudarte a alcanzar tus metas y maximizar tu potencial. Nuestros programas de mentoría están diseñados para brindarte la orientación y el apoyo necesarios para enfrentar desafíos profesionales y alcanzar el éxito en tu vida laboral.
+    return (
+        <div className="flex flex-col h-full">
+            <div className={style.container}>
+                <div className=" mx-auto flex items-center justify-end mt-8 mr-20 h-full ">
+                    <div className='mb-10 bg-zinc-300 bg-opacity-70 backdrop-blur-full pr-28 pl-10 pt-20 pb-20 w-1/3 rounded-tl-full rounded-tr-full rounded-bl-full rounded-br-full'>
+                        <h1 className='text-center text-green-700 font-bold text-6xl mb-5'>Mentoría</h1>
+                        <p className='font-semibold'>Ofrecemos servicios de mentoring personalizados para ayudarte a alcanzar tus metas y
+                        maximizar tu potencial. Nuestros programas de mentoría están diseñados para brindarte la
+                        orientación y el apoyo necesarios para enfrentar desafíos profesionales y alcanzar el éxito
+                        en tu vida laboral.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex space-x-6 mt-20">
+                <div className="flex-1 text-center m-10 block rounded-lg bg-transparent bg-opacity-50 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                    <div className="p-6">
+      <h5 className="mb-2 text-3xl leading-tight text-green-700 font-bold">
+        <i className="fas fa-star mr-2 text-4xl"></i> Mentoría Standard
+      </h5>
+      <p className="mb-4 text-base text-neutral-600 text-sm">
+        El pack de Mentoría Standard es un punto de partida perfecto para aquellos que buscan un acompañamiento efectivo en su desarrollo profesional. Incluye 4 sesiones online de 1 hora cada una. Durante estas sesiones, trabajaremos juntos para identificar tus objetivos, trazar un plan de acción y superar obstáculos que puedan surgir en el camino. El ritmo de la mentoría se adapta a tus necesidades, permitiéndote avanzar a tu propio ritmo. La duración típica de este programa es de 1 a 2 meses, asegurándonos de que logres un progreso significativo en tu trayectoria profesional.
       </p>
-      </div>
-      <div
-  class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-  <a href="#!">
-    <img
-      class="rounded-t-lg"
-      src="https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg"
-      alt="" />
-  </a>
-  <div class="p-2">
-    <h5
-      class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-      Card title
-    </h5>
-    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-      Some quick example text to build on the card title and make up the
-      bulk of the card's content.
-    </p>
+    </div>
+  </div>
+  <div className="flex-1 text-center m-10 block rounded-lg bg-transparent bg-opacity-50 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+    <div className="p-6">
+      <h5 className="mb-2 text-3xl leading-tight text-yellow-600 dark:text-neutral-50">
+        <i className="fas fa-crown mr-2 text-4xl"></i> Mentoría Premium
+      </h5>
+      <p className="mb-4 text-base text-neutral-600 text-sm">
+        El pack de Mentoría Premium es un programa más completo y detallado, ideal para aquellos
+        que desean un compromiso más prolongado y profundo. Incluye 6 sesiones online de 1 hora
+        cada una, permitiéndome brindarte un apoyo aún más personalizado. Durante estas
+        sesiones, nos enfocaremos en desarrollar tus habilidades clave, trabajar en el
+        perfeccionamiento de tus fortalezas y abordar áreas de mejora específicas. El ritmo de la
+        mentoría se ajustará a tus requerimientos y circunstancias únicas, garantizando que
+        alcances tus objetivos de la manera más efectiva posible. La duración de este programa se
+        extiende normalmente de 2 a 3 meses, lo que nos permitirá profundizar en el crecimiento y
+        desarrollo de tu carrera.
+      </p>
+    </div>
   </div>
 </div>
-    </div>
-    </div>
-  );
-};
+        </div>
+    );
+}
 
 export default Mentoring;

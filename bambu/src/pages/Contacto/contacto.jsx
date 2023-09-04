2be4fb8791d1e3f334d6ddf85157106c78c0d643
mauro_genna <gennamauro18@gmail.com>
2023-09-04 18:30:03 -0300

    Mensaje de exito en Contacto

--- bambu/src/pages/Contacto/contacto.jsx
+++ bambu/src/pages/Contacto/contacto.jsx
@@ -1,33 +1,77 @@
 import Image from "next/image";
-import ImagenContacto from "../../Assets/contacto.jpg";
 import React from 'react';
 import styles from "./contacto.module.css"
 import { useForm, ValidationError } from '@formspree/react';
 import Link from "next/link";
 
 const Contacto = () => {
-    const [state, handleSubmit] = useForm("xjvqrvwq");
+    const [state, handleSubmit] = useForm("mvojgaqr"); // xjvqrvwq
     if (state.succeeded) {
         return (
-            <div>
-                <p>
-                    Gracias por confiar en Bambú!
-                </p>
-                <p>
-                    Pronto nos pondremos en contacto contigo!
-                </p>
+            <div className="flex flex-col h-full">
+
+                <div className={styles.header}>
+                    <div className="lg:flex lg:justify-end lg:items-center h-full">
+                        <div className="top-0 bg-zinc-300 bg-opacity-70 backdrop-blur-sm lg:p-12 w-full lg:w-1/3 mr-5 rounded-tl-none rounded-tr-full rounded-bl-full rounded-br-none">
+                            <h1 className='text-center font-bold text-4xl mb-5'>
+                                Conversa Con Nosotros <span className='text-lime-700'>¡Estamos Aquí Para Escucharte!</span>
+                            </h1>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="flex-grow flex">
+                    <div className="w-1/2 p-8">
+                        <h1 className="mb-4 text-center text-lg font-medium text-gray-800">
+                            Contactate con nosotros
+                        </h1>
+                        <div className="flex justify-center">
+                            <a href="https://api.whatsapp.com/send?phone=3516578866" target="_blank" rel="noopener noreferrer" className={styles.whatsappIcon}>
+                                <i className="fab fa-whatsapp"></i>
+                            </a>
+                            <a href="" className={styles.linkedinIcon}>
+                                <i className="fab fa-linkedin"></i>
+                            </a>
+                            <a href="https://instagram.com/cordobabambu?igshid=ZGUzMzM3NWJiOQ==" className={styles.instagramIcon}>
+                                <i className="fab fa-instagram"></i>
+                            </a>
+                        </div>
+                        <div className="w-full text-center mt-10">
+                            <hr className="w-1/2 mx-auto border-t-2 border-blue-500" />
+                            <h3 className="mt-5 text-center text-lg font-medium text-gray-800">Realiza nuestro diagnóstico aquí</h3>
+                            <Link href="/Contacto/Diagnostico/diagnostico">
+                                <button
+                                    type="button"
+                                    className="inline-block mt-5 rounded-full border-2 border-success px-6 pb-[6px] pt-2 text-md font-medium uppercase leading-normal text-success transition duration-150 ease-in-out hover:border-success-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-success-600 focus:border-success-600 focus:text-success-600 focus:outline-none focus:ring-0 active:border-success-700 active:text-success-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+                                    data-te-ripple-init>Formulario Diagnóstico</button>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="w-1/2 p-8 text-center">
+                        <div class="bg-white p-8 rounded-3xl shadow-md bg-sky-200 animated animate__fadeIn">
+                            <h2 className="text-2xl font-semibold mb-4">¡Tu mensaje ha sido enviado con éxito!</h2>
+                            <p className="text-gray-700">Gracias por ponerte en contacto con nosotros. Hemos recibido tu mensaje y nos pondremos en contacto contigo en breve. Valoramos tu interés en nuestra empresa y estamos ansiosos por ayudarte.</p>
+                            <p className="text-gray-700">Si tienes alguna pregunta adicional o necesitas asistencia inmediata, no dudes en llamarnos al <span className="text-green-700 hover:underline">
+                                <a href="https://api.whatsapp.com/send?phone=3516578866" target="_blank" rel="noopener noreferrer"><i className="fab fa-whatsapp"></i> 3516578866 </a></span></p>
+                            <p className="text-gray-700">¡Ten un gran día!</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
     return (
         <div className="flex flex-col h-full">
 
-            <Image
-                src={ImagenContacto}
-                alt="Contact Image"
-                className={styles.customImage}
-                layout="responsive"
-            />
+            <div className={styles.header}>
+                <div className="lg:flex lg:justify-end lg:items-center h-full">
+                    <div className="top-0 bg-zinc-300 bg-opacity-70 backdrop-blur-sm lg:p-12 w-full lg:w-1/3 mr-5 rounded-tl-none rounded-tr-full rounded-bl-full rounded-br-none">
+                        <h1 className='text-center font-bold text-4xl mb-5'>
+                            Conversa Con Nosotros <span className='text-lime-700'>¡Estamos Aquí Para Escucharte!</span>
+                        </h1>
+                    </div>
+                </div>
+            </div>
 
             <div className="flex-grow flex">
                 <div className="w-1/2 p-8">

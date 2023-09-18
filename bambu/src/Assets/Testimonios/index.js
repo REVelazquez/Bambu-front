83a760cb217116bf44abc30e2244ceec4370ece7
mauro_genna <gennamauro18@gmail.com>
2023-09-18 11:54:14 -0300

    actualizacion de carpetas para deploy

--- bambu/src/Assets/Testimonios/index.js
+++
@@ -1,9 +0,0 @@
-import Candelaria from './CandelariaLarrosa.jpg'
-import Clarisa from './ClarisaBalbo.jpg'
-import Julieta from './JulietaActis.jpg'
-import Marcos from './MarcosGutierrez.jpg'
-import Melina from './MelinaSerra.jpg'
-import Sergio from './SergioDiamonte.jpg'
-
-//para exportar
-export const images = { Candelaria, Clarisa, Julieta, Marcos, Melina, Sergio }

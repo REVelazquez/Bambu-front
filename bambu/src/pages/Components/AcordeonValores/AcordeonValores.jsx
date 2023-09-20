1a0de20a4e3c6d298c4c98fa0562e00fdff7902c
Adrianl93 <laborde.adrian2012@gmail.com>
2023-09-20 18:36:17 -0300

    about responsive 3

--- bambu/src/pages/Components/AcordeonValores/AcordeonValores.jsx
+++ bambu/src/pages/Components/AcordeonValores/AcordeonValores.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { valores } from '../../../../public/Assets/about/aboutText'
+import {valores} from '../../../../public/Assets/about/aboutText'
 
 const AcordeonValores = () => {
   const [activeItems, setActiveItems] = useState(new Array(valores.length).fill(false));
@@ -12,35 +12,34 @@ const AcordeonValores = () => {
   };
 
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 lg:flex lg:flex-wrap w-11/12 mx-auto animate__animated animate__fadeIn">
-      {/*   */}
+    <section className="grid md:grid-cols-1 w-full md:w-1/2 mx-auto animate__animated animate__fadeIn">
       {valores.map((valores, index) => (
         <section
           className={`flex flex-col md:flex-row mb-4 mr-4 ml-4 p-4 bg-transparent rounded-lg shadow-md lg:shadow-lg dark:bg-neutral-700 transition duration-300 ${activeItems[index] ? 'bg-green-300' : ''}`}
           key={index}
         >
           <div className="flex flex-col w-full">
-            <button
-              onClick={() => handleClick(index)}
-              className="py-1 px-2 rounded-md bg-transparent text-black"
-            >
-              <div className="flex justify-between">
-                <h2 className="text-xl font-semibold">{valores.name}</h2>
+            <div className="flex justify-between">
+              <h2 className="text-sm sm:text-lg md:text-xl lg:text-2xl xl:text-2xl font-semibold text-center md:text-center lg:text-center">{valores.name}</h2>
+              <button
+                onClick={() => handleClick(index)}
+                className="py-1 px-2 rounded-md bg-transparent text-white"
+              >
                 {activeItems[index] ?
                   <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z" /></svg>
                   :
                   <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" /></svg>
                 }
+              </button>
+            </div>
+            {activeItems[index] && (
+              <div>
+                <hr className="w-full mx-auto mt-2 border-t-2 border-gray-200" />
+                <p className="w-full mx-auto mt-2 text-center text-lg animate__animated animate__fadeIn">
+                  {valores.description}
+                </p>
               </div>
-              {activeItems[index] && (
-                <div>
-                  <hr className="w-full mx-auto mt-2 border-t-2 border-gray-200" />
-                  <p className="w-full mx-auto mt-2 text-center text-lg animate__animated animate__fadeIn">
-                    {valores.description}
-                  </p>
-                </div>
-              )}
-            </button>
+            )}
           </div>
         </section>
       ))}
--- bambu/src/pages/Components/CardsAbout/Card.jsx
+++ bambu/src/pages/Components/CardsAbout/Card.jsx
@@ -14,14 +14,14 @@ const Card = ({ id, image, front, back }) => {
 
   return (
     <div key={'a' + id}
-      className="w-96 h-60 mx-4 my-4  transition-all duration-500 ease-in-out transform hover:scale-105 flex-1 text-center m-10 block rounded-lg bg-transparent bg-opacity-50 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+      className="w-80 h-60  md:w-80 lg:w-96 mx-auto my-4 items-center transition-all duration-500 ease-in-out transform hover:scale-105 flex-1 text-center m-10 block rounded-lg bg-transparent bg-opacity-50 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave} >
-      {show ? <span key={'c' + id} className="max-w-full h-64  bg-green-500 flex rounded-md   items-center justify-center">
+      {show ? <span key={'c' + id} className="max-w-full h-64  bg-green-500 flex rounded-md  items-center justify-center">
         <p key={'bt' + id} className="mx-4 text-xl text-white animated animate__fadeIn">{back}</p></span>
         : <div key={'b' + id} className="w-80 h-64 bg-transparent flex rounded-md border-solid   items-center justify-center round-">
-          <Image key={'i' + id} src={image} className="w-16 h-16 ml-6 mr-2" />
-          <h1 key={'ft' + id} className="w-40 mr-4 font-bold font-black text-2xl">{front}</h1>
+          <Image key={'i' + id} src={image} className="w-16 h-16  mr-2" />
+          <h1 key={'ft' + id} className="w-1/2 sm:w-40 mr-2 sm:mr-4 font-bold font-black text-lg sm:text-2xl">{front}</h1>
         </div>}
 
 
